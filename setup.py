8453e79d7db273c5a23cfeac8941f9f9f29d6a36
Cedric Bou <cedric.boufflers@gmail.com>
2016-11-17 23:33:22 +0100

    Project structure with test and lint

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Conventional Setup.py file"""
+from setuptools import setup
+
+setup(name='fiddlewith',
+      version='1.0',
+      py_modules=['fiddlewith'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+     )
---
+++ tests/test_foo.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+import foo
+
+class TestFoo(TestCase):
+   def test_is_string(self):
+      self.assertTrue(True)
+
